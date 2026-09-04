be4ed39593d876aad5f38684786b4f45c9a890ab
maryxz09 <maschiariconceicaom@gmail.com>
2026-09-04 09:27:14 -0300

    Add files via upload

--- alura/Lista/script.js
+++ alura/Lista/script.js
@@ -1,31 +1,12 @@
-let maquiagem =[
-  "<li>"+maquiagem[0]+"</li>"+
-    "<li>"+maquiagem[1]+"</li>"+
-    "<li>"+maquiagem[2]+"</li>"+
-    "<li>"+maquiagem[3]+"</li>"+
-    "<li>"+maquiagem[4]+"</li>"+
-    "<li>"+maquiagem[5]+"</li>"+
-    "<li>"+maquiagem[6]+"</li>"+
-    "<li>"+maquiagem[7]+"</li>"+
-    "<li>"+maquiagem[8]+"</li>"+
-    "<li>"+maquiagem[9]+"</li>"+
-    "<li>"+maquiagem[10]+"</li>"+
-    "<li>"+maquiagem[11]+"</li>";
+const lista = document.getElementById('listaMaquiagem');
+const botao = document.getElementById('botao');
 
-   
-];
-
-function  mostrarLista(){
- alert("Funcionou!");
- let  = document.getElementByld("listaMaquiagem");
-
-    lista.innerHTML=
+botao.addEventListener('click',() =>{
+ 
+    if(lista.style.display === 'none' ||Maquiagem.style.display ===''){
+        lista.style.display ='block';
+    }else{
+        lista.style.display = 'none';
+    }      
     
-
-
-    for (let i =0;i < maquiagem.length;i++){
-
-        lista.innerHTML +="<li>" +i+ "-" + maquiagem[i] + "</li>";
-        lista.appendChild(item);
-    }
-}
+});
