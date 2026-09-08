8a1f7c4399406c0f2572a6ab6db2ae6328b0bfc2
maryxz09 <maschiariconceicaom@gmail.com>
2026-09-08 11:27:54 -0300

    Add files via upload

---
+++ alura/3411-praticando-logica-projeto_inicial/ingresso/js/app.js
@@ -0,0 +1,23 @@
+function comprar(){
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+   if(tipo.value =='pista'){
+    comprarPista(qtd);
+   }else if (tipo.value == 'superior'){
+    comprarSuperior(qtd);
+   }else{
+    comprarInferior(qtd);
+   }
+}
+
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if(qtd > qtdPista){
+       alert('Quantidade indisponivel para tipo pista');
+    }else{
+        qtdPista = qtdPista -qtd ;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
